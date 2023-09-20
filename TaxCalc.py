728a4e5e4319abd7a0b79d3a8509726263d441b1
UnnormalPerson <135814712+UnnormalPerson@users.noreply.github.com>
2023-09-21 07:58:56 +0800

    .

--- TaxCalc.py
+++ TaxCalc.py
@@ -1,1 +1,1 @@
-print ('Before anything happens, please know this is Singapore tax, so if you don't live in SG and want to calculate your tax, make a pull issue and I wil try.')
+print ("Before anything happens, please know this is Singapore tax, so if you don't live in SG and want to calculate your tax, make a pull issue and I wil try.")
